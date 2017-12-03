3668428e562cdabbcafcd2370f07fc5af73724fd
machuanjia <yanshi0429@126.com>
2017-12-03 17:23:03 +0800

    add

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <img src="assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss">
+  // Import Main styles for this application
+  @import "./scss/style";
+</style>
+
